fcbdfff068f71e80c4c7640a3ede15449c8fe369
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-05-14 18:21:43 -0300

    iniciando com python e aprendendo a sintaxe mais avançada

---
+++ 02.sintaxe_estrutura_python/02.avancando_sintaxe.py
@@ -0,0 +1,10 @@
+# Recebendo dados com input
+nome_usuario = input("Digite o seu nome: ")
+print(f"O seu nome é {nome_usuario}")
+
+temperatura = 32
+
+classe = "Arqueiro"
+
+numero = 10
+print(type(numero))
