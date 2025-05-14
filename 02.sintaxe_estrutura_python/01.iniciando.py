2e32c6a10bd3877a3ba6d867c3c119027e768610
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-05-14 18:21:34 -0300

    iniciando com python e aprendendo a sintaxe

---
+++ 02.sintaxe_estrutura_python/01.iniciando.py
@@ -0,0 +1,29 @@
+print("Hello World")
+
+def saudacao():
+    print("Minha saudação")
+    
+if __name__ == "__main__":
+    saudacao()
+
+if 15 > 10:
+    print("15 realmente é maior que 10")
+    print("Operação finalizada")
+
+if 70 > 18:
+    print("Você é maior de idade")
+    if 70 > 65:
+        print("Está na hora de se aposentar")
+    print("Você é idoso")
+    
+    
+# Comentário
+
+# Função print
+print("Exibir uma mensagem")
+
+print("Exibir", "Mensagem")
+
+print("Laranja", "Maçã", "Banana", sep=" - ")
+
+print("Carregando", end="...")
