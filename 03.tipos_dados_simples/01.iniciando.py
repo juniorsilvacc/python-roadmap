e76fc02a53e8cd594965a304b3db2f4c031422b2
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-05-14 18:22:08 -0300

    iniciando com tipos de dados simples

---
+++ 03.tipos_dados_simples/01.iniciando.py
@@ -0,0 +1,38 @@
+# Números inteiros
+numero1 = 42
+numero2 = 50
+
+print (f"Número {numero1}, {numero2}")
+
+print(type(numero1))
+
+valor_absoluto = abs(numero1)
+print(valor_absoluto)
+
+expo = pow(numero1, 2)
+print(expo)
+
+soma = numero1 + numero2
+print(f"Soma: {soma}")
+
+# Artimetrica
+a = 160
+b = 15
+
+soma = a + b
+multiplicacao = a * b
+divisao_inteira = a // b # Divisão inteira
+divisao_normal = a / b # Divisão normal
+subtracao = a - b
+resto = a % b
+
+print("S: ", soma)
+print("M: ", multiplicacao)
+print("D - inteira: ", divisao_inteira)
+print("D - normal: ", divisao_normal)
+print("Sub: ", subtracao)
+print("Resto: ", resto)
+
+contador = 1
+contador += 10
+print(contador)
