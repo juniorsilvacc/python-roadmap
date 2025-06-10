2d53a6b3e1b9b63f89b9f28478eb10099a3a6abf
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-06-10 04:47:57 -0300

    método string split

--- 05.strings/3.metodos_string.py
+++ 05.strings/3.metodos_string.py
@@ -12,8 +12,12 @@ print(espacos.strip())
 
 # Substituição de caracteres
 subs_carecteres = "Hoje o dia será ótimo"
-
 print(subs_carecteres.replace("o dia", "a noite"))
 
+# Split para separar por ","
 dados_separados = "nome,sobrenome,idade,email"
 print(dados_separados.split(','))
+
+# Acessando valor em lista
+palavras_lista = ['Oi', 'Boa', 'Noite']
+print(palavras_lista[1])
