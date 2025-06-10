fd904e59646844409c6a8775b7e05534428fa2b1
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-06-10 04:20:06 -0300

    métodos strings

---
+++ 05.strings/3.metodos_string.py
@@ -0,0 +1,19 @@
+#  Formatação
+frase = "Python é muiTo boM"
+
+print(frase.lower())
+print(frase.upper())
+print(frase.capitalize())
+
+# Remoção de espaços
+espacos = " Remocao de espaços  "
+
+print(espacos.strip())
+
+# Substituição de caracteres
+subs_carecteres = "Hoje o dia será ótimo"
+
+print(subs_carecteres.replace("o dia", "a noite"))
+
+dados_separados = "nome,sobrenome,idade,email"
+print(dados_separados.split(','))
