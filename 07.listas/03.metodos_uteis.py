0e3dfb7471cb8e8f7d7b746ed004e876b0b0ea1b
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-06-11 20:47:04 -0300

    metodos uteis em listas

---
+++ 07.listas/03.metodos_uteis.py
@@ -0,0 +1,12 @@
+frutas = ["maça", "banana", "laranja", "melancia", "morango"]
+
+print(frutas.count("maça"))
+print(frutas.count("abacate"))
+
+print(frutas.index("maça"))
+print(frutas.index("melancia"))
+print(frutas.index("morango"))
+
+# Limpar listar
+frutas.clear()
+print(frutas)
