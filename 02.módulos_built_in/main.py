604b79bc64ecf2a7ba990b825b1ee758190d8a97
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-08-20 14:40:09 -0300

    módulos built in

---
+++ 02.módulos_built_in/main.py
@@ -0,0 +1,13 @@
+# Programa Principal
+import math_operations
+import strings_utils
+from math_operations import multiply, divide
+
+print(math_operations.sum(5, 3))
+print(math_operations.subtract(5, 2))
+print(multiply(5, 4))
+print(divide(8, 2))
+
+print(strings_utils.capitalize("python"))
+print(strings_utils.reverse_string("python"))
+print(strings_utils.count("python"))
