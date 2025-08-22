e70c68ea1b293b0e51ecf93e14babb4c73298dd7
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-08-22 13:42:24 -0300

    refatorando o código

--- 04.oo_projeto/.temp/biblioteca.py
+++ 04.oo_projeto/.temp/biblioteca.py
@@ -1,10 +1,10 @@
 class Biblioteca:
-    bibliotecas = []
-
+    bibliotecas = [] # atributo de classe (compartilhado por todas as instâncias)
+    
     def __init__(self, nome):
         self.nome = nome
         self._ativo = False
-
+    
     def __str__(self):
         return self.nome
     
@@ -18,11 +18,26 @@ class Biblioteca:
         for biblioteca in cls.bibliotecas:
             print(f"{biblioteca.nome} - {biblioteca.ativo}")
     
+    # setter: altera o valor de _ativo
     def alterna_estado(self):
-        """setter: Alterna o estado da biblioteca"""
+        """Alterna o estado da biblioteca"""
         self._ativo = not self._ativo
     
+    # getter: retorna o estado de forma legível
     @property
     def ativo(self):
-        """getter: Propriedade que permite acessar o estado da biblioteca"""
+        """Propriedade que permite acessar o estado da biblioteca"""
         return "ativado" if self._ativo else "desativado"
+
+# Criando instâncias
+b1 = Biblioteca("Biblioteca da Cidade")
+b2 = Biblioteca("Biblioteca do Shopping")
+
+b1.registrar()
+b1.alterna_estado()
+
+b2.registrar()
+b2.alterna_estado()
+
+# Chamando classmethod sem precisar de instância
+Biblioteca.listar_bibliotecas()
