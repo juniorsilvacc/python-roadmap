1c60a4ffdb811472c799b0c4204b2d51ca4bab35
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-08-23 03:52:38 -0300

    criando outra classe e gerenciando avaliações

--- 04.oo_projeto/app.py
+++ 04.oo_projeto/app.py
@@ -6,6 +6,12 @@ biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 biblioteca_cidade.registrar()
 biblioteca_shopping.registrar()
 
+biblioteca_cidade.receber_avaliacao("Junior Silva", 9.5)
+biblioteca_cidade.receber_avaliacao("Cícero Júnior", 10.0)
+
+biblioteca_shopping.receber_avaliacao("Cícero Júnior", 8.0)
+biblioteca_shopping.receber_avaliacao("Cícero Júnior", 7.0)
+
 def main():
     Biblioteca.listar_bibliotecas()
 
--- 04.oo_projeto/modelos/biblioteca.py
+++ 04.oo_projeto/modelos/biblioteca.py
@@ -1,9 +1,12 @@
+from modelos.avaliacao import Avaliacao
+
 class Biblioteca:
     bibliotecas = []
 
     def __init__(self, nome):
         self.nome = nome
         self._ativo = False
+        self._avaliacao = []
 
     def __str__(self):
         return self.nome
@@ -15,8 +18,9 @@ class Biblioteca:
     @classmethod
     def listar_bibliotecas(cls):
         """Mostra todas as bibliotecas registradas"""
+        print(f"{'Nome da biblioteca'.ljust(25)} | {'Nota média'.ljust(25)} | {'Status'.ljust(25)}")
         for biblioteca in cls.bibliotecas:
-            print(f"{biblioteca.nome} - {biblioteca.ativo}")
+            print(f"{biblioteca.nome.ljust(25)} | {str(biblioteca.media_avaliacoes).ljust(25)} | {biblioteca.ativo.ljust(25)}")
     
     def alterna_estado(self):
         """setter: Alterna o estado da biblioteca"""
@@ -26,3 +30,16 @@ class Biblioteca:
     def ativo(self):
         """getter: Propriedade que permite acessar o estado da biblioteca"""
         return "ativado" if self._ativo else "desativado"
+    
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+    
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-'
+        else:
+            soma = sum(avaliacao._nota for avaliacao in self._avaliacao) # Pegar cada nota, para cada avaliação que esteja na lista de avaliações
+            media = round(soma / len(self._avaliacao), 1)
+            return media
